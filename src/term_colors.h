f7851a539466c9932bdebec2591f616ac56e5600
Tomáš Klapka <tomas@idni.org>
2024-03-11 18:46:23 +0100

    term_colors: add modifier and clear modifier shortcuts; refactor get and getc to operator[] and operator() only

--- src/term_colors.h
+++ src/term_colors.h
@@ -13,9 +13,26 @@
 #ifndef __IDNI_TERM_COLORS_H__
 #define __IDNI_TERM_COLORS_H__
 
-// class term_colors TC. constructor takes argument (bool colors_enabled = true);
-// use: os << TC.BLUE() << "blue text" << TC.CLEAR();
-// or:  os << TC(BRIGHT, WHITE) << "bright and white text" << TC.CLEAR();
+// struct term_colors
+// this struct contains methods to generate escape codes for terminal colors
+// and modifiers. Depending on the member variable 'enabled' it will return
+// the escape code or an empty string.
+//
+// constructor takes an optional argument (bool colors_enabled = true);
+// and it is possible to change it later with the methods enable(), disable(),
+// toggle() or set(bool e).
+//
+// term_colors::operator[color c] returns the escape code for the color,
+// term_colors::operator(Colors... colors) returns the whole color code and
+// it accepts any number of color codes as arguments.
+//
+// Example usage:
+//     term_colors TC;
+//     cout << TC.BLUE() << "blue text" << TC.CLEAR();
+//     cout << TC(BRIGHT, WHITE) << "bright and white text" << TC.CLEAR();
+// or manually compose the color code:
+//     cout << TC[COLOR] << TC[BG_GREEN] << TC[AND] << TC[WHITE] << TC[EOC]
+//            << "green background and white text" << TC[CLEAR];
 
 #include <vector>
 #include <string>
@@ -41,56 +58,73 @@ using color = enum {
 
 struct colors {
 	colors(bool colors_enabled = true) : enabled(colors_enabled) {}
+
+	bool enabled = true;
 	bool enable()    { return enabled = true; }
 	bool disable()   { return enabled = false; }
 	bool toggle()    { return enabled = !enabled; }
 	bool set(bool e) { return enabled = e; }
-	bool enabled = true;
-	std::string COLOR()            { return get(color::COLOR); }
-	std::string AND()              { return get(color::AND); }
-	std::string EOC()              { return get(color::EOC); }
-	std::string BRIGHT()           { return get(color::BRIGHT); }
-	std::string UNDERLINED()       { return get(color::UNDERLINED); }
-	std::string BLINK()            { return get(color::BLINK); }
-	std::string CLEAR()            { return getc(color::CLEAR); }
-	// foregrounds
-	std::string DEFAULT()          { return getc(color::DEFAULT); }
-	std::string BLACK()            { return getc(color::BLACK); }
-	std::string RED()              { return getc(color::RED); }
-	std::string GREEN()            { return getc(color::GREEN); }
-	std::string YELLOW()           { return getc(color::YELLOW); }
-	std::string BLUE()             { return getc(color::BLUE); }
-	std::string MAGENTA()          { return getc(color::MAGENTA); }
-	std::string CYAN()             { return getc(color::CYAN); }
-	std::string LIGHT_GRAY()       { return getc(color::LIGHT_GRAY); }
-	std::string DARK_GRAY()        { return getc(color::DARK_GRAY); }
-	std::string LIGHT_RED()        { return getc(color::LIGHT_RED); }
-	std::string LIGHT_GREEN()      { return getc(color::LIGHT_GREEN); }
-	std::string LIGHT_YELLOW()     { return getc(color::LIGHT_YELLOW); }
-	std::string LIGHT_BLUE()       { return getc(color::LIGHT_BLUE); }
-	std::string LIGHT_MAGENTA()    { return getc(color::LIGHT_MAGENTA); }
-	std::string LIGHT_CYAN()       { return getc(color::LIGHT_CYAN); }
-	std::string WHITE()            { return getc(color::WHITE); }
-	// backgrounds
-	std::string BG_DEFAULT()       { return getc(color::BG_DEFAULT); }
-	std::string BG_BLACK()         { return getc(color::BG_BLACK); }
-	std::string BG_RED()           { return getc(color::BG_RED); }
-	std::string BG_GREEN()         { return getc(color::BG_GREEN); }
-	std::string BG_YELLOW()        { return getc(color::BG_YELLOW); }
-	std::string BG_BLUE()          { return getc(color::BG_BLUE); }
-	std::string BG_MAGENTA()       { return getc(color::BG_MAGENTA); }
-	std::string BG_CYAN()          { return getc(color::BG_CYAN); }
-	std::string BG_LIGHT_GRAY()    { return getc(color::BG_LIGHT_GRAY); }
-	std::string BG_DARK_GRAY()     { return getc(color::BG_DARK_GRAY); }
-	std::string BG_LIGHT_RED()     { return getc(color::BG_LIGHT_RED); }
-	std::string BG_LIGHT_GREEN()   { return getc(color::BG_LIGHT_GREEN); }
-	std::string BG_LIGHT_YELLOW()  { return getc(color::BG_LIGHT_YELLOW); }
-	std::string BG_LIGHT_BLUE()    { return getc(color::BG_LIGHT_BLUE); }
-	std::string BG_LIGHT_MAGENTA() { return getc(color::BG_LIGHT_MAGENTA); }
-	std::string BG_LIGHT_CYAN()    { return getc(color::BG_LIGHT_CYAN); }
-	std::string BG_WHITE()         { return getc(color::BG_WHITE); }
 
-	const std::string& get(color c) {
+	// escape controls COLOR = start, AND = separator, EOC = end of color
+	std::string COLOR()            { return (*this)[color::COLOR]; }
+	std::string AND()              { return (*this)[color::AND]; }
+	std::string EOC()              { return (*this)[color::EOC]; }
+
+	// clear/reset all color settings
+	std::string CLEAR()            { return (*this)(color::CLEAR); }
+	// set modifiers
+	std::string BRIGHT()           { return (*this)(color::BRIGHT); }
+	std::string DIM()              { return (*this)(color::DIM); }
+	std::string UNDERLINED()       { return (*this)(color::UNDERLINED); }
+	std::string BLINK()            { return (*this)(color::BLINK); }
+	std::string REVERSE()          { return (*this)(color::REVERSE); }
+	std::string HIDDEN()           { return (*this)(color::HIDDEN); }
+	// clear modifiers
+	std::string CLEAR_BRIGHT()     { return (*this)(color::CLEAR_BRIGHT); }
+	std::string CLEAR_DIM()        { return (*this)(color::CLEAR_DIM); }
+	std::string CLEAR_UNDERLINED() { return (*this)(color::CLEAR_UNDERLINED); }
+	std::string CLEAR_BLINK()      { return (*this)(color::CLEAR_BLINK); }
+	std::string CLEAR_REVERSE()    { return (*this)(color::CLEAR_REVERSE); }
+	std::string CLEAR_HIDDEN()     { return (*this)(color::CLEAR_HIDDEN); }
+	// foregrounds
+	std::string DEFAULT()          { return (*this)(color::DEFAULT); }
+	std::string BLACK()            { return (*this)(color::BLACK); }
+	std::string RED()              { return (*this)(color::RED); }
+	std::string GREEN()            { return (*this)(color::GREEN); }
+	std::string YELLOW()           { return (*this)(color::YELLOW); }
+	std::string BLUE()             { return (*this)(color::BLUE); }
+	std::string MAGENTA()          { return (*this)(color::MAGENTA); }
+	std::string CYAN()             { return (*this)(color::CYAN); }
+	std::string LIGHT_GRAY()       { return (*this)(color::LIGHT_GRAY); }
+	std::string DARK_GRAY()        { return (*this)(color::DARK_GRAY); }
+	std::string LIGHT_RED()        { return (*this)(color::LIGHT_RED); }
+	std::string LIGHT_GREEN()      { return (*this)(color::LIGHT_GREEN); }
+	std::string LIGHT_YELLOW()     { return (*this)(color::LIGHT_YELLOW); }
+	std::string LIGHT_BLUE()       { return (*this)(color::LIGHT_BLUE); }
+	std::string LIGHT_MAGENTA()    { return (*this)(color::LIGHT_MAGENTA); }
+	std::string LIGHT_CYAN()       { return (*this)(color::LIGHT_CYAN); }
+	std::string WHITE()            { return (*this)(color::WHITE); }
+	// backgrounds
+	std::string BG_DEFAULT()       { return (*this)(color::BG_DEFAULT); }
+	std::string BG_BLACK()         { return (*this)(color::BG_BLACK); }
+	std::string BG_RED()           { return (*this)(color::BG_RED); }
+	std::string BG_GREEN()         { return (*this)(color::BG_GREEN); }
+	std::string BG_YELLOW()        { return (*this)(color::BG_YELLOW); }
+	std::string BG_BLUE()          { return (*this)(color::BG_BLUE); }
+	std::string BG_MAGENTA()       { return (*this)(color::BG_MAGENTA); }
+	std::string BG_CYAN()          { return (*this)(color::BG_CYAN); }
+	std::string BG_LIGHT_GRAY()    { return (*this)(color::BG_LIGHT_GRAY); }
+	std::string BG_DARK_GRAY()     { return (*this)(color::BG_DARK_GRAY); }
+	std::string BG_LIGHT_RED()     { return (*this)(color::BG_LIGHT_RED); }
+	std::string BG_LIGHT_GREEN()   { return (*this)(color::BG_LIGHT_GREEN); }
+	std::string BG_LIGHT_YELLOW()  { return (*this)(color::BG_LIGHT_YELLOW); }
+	std::string BG_LIGHT_BLUE()    { return (*this)(color::BG_LIGHT_BLUE); }
+	std::string BG_LIGHT_MAGENTA() { return (*this)(color::BG_LIGHT_MAGENTA); }
+	std::string BG_LIGHT_CYAN()    { return (*this)(color::BG_LIGHT_CYAN); }
+	std::string BG_WHITE()         { return (*this)(color::BG_WHITE); }
+
+	// return the code for the color or empty string if colors disabled
+	const std::string& operator[](color c) {
 		static const std::vector<std::string> color_code{
 			"\x1b[", ";", "m", "0",
 			"1", "2", "4",
@@ -109,24 +143,17 @@ struct colors {
 		static const std::string no_code{};
 		return enabled ? color_code[c] : no_code;
 	}
-	std::string getc(color c) {
-		std::stringstream ss;
-		return ss << COLOR() << get(c) << EOC(), ss.str();
+	// return the whole color code or empty string if colors disabled
+	template<typename... Colors>
+	std::string operator()(Colors... colors) {
+		bool first = true;
+		std::stringstream ss; ss << COLOR();
+		auto add = [this, &ss, &first](const auto& c) {
+			if (!first) ss << AND(); else first = false;
+			ss << (*this)[c];
+		};
+		return (add(colors), ...), ss << EOC(), ss.str();
 	}
-	std::string getc(color c1, color c2) {
-		std::stringstream ss;
-		return ss << COLOR() << get(c1) << AND() << get(c2) << EOC(),
-			ss.str();
-	}
-	std::string getc(color c1, color c2, color c3) {
-		std::stringstream ss;
-		return ss << COLOR() << get(c1)	<< AND() << get(c2)
-			<< AND() << get(c3) << EOC(), ss.str();
-	}
-	std::string operator()(color c) { return getc(c); }
-	std::string operator()(color c1, color c2) { return getc(c1, c2); }
-	std::string operator()(color c1, color c2, color c3) {
-						return getc(c1, c2, c3); }
 };
 
 } // idni::term namespace
