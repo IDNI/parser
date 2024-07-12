7cc1969cd95cf3377c4d54ff2600cf5111226907
UmarInvent <umar@idni.org>
2024-07-12 18:06:22 +0500

    one map for all: gc, handles

--- src/bintree.cpp
+++ src/bintree.cpp
@@ -2,26 +2,4 @@
 
 namespace idni {
 
-std::unordered_map<tref, htree::wp> htree::M;
-
-htree::sp htree::get(tref h) {
-    DBG(assert(h != NULL);)
-    if (h == NULL) return null();
-    auto res = M.emplace(h, wp()); //done with one search
-    sp ret; 
-    if (res.second) 
-        res.first->second = ret = sp(new htree(h));
-    assert(!res.first->second.expired());
-    return res.first->second.lock(); 
-}
-
-void htree::dump() {
-	std::cout<<"-----\n";
-	std::cout<<"S:"<<M.size()<<"\n";
-	for (auto &x : M)
-		std::cout<<x.first << " " <<&x.second <<
-		" "<< x.second.lock()->get() <<"\n";
-	std::cout<<"-----\n";
-}
-
 }
