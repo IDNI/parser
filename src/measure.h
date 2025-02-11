810d76ca2927f88e1928710149fddca879e3f424
castrod <david.castroesteban@gmail.com>
2025-02-11 16:38:04 +0100

    updating measures.h

--- src/measure.h
+++ src/measure.h
@@ -1,22 +1,12 @@
-// LICENSE
-// This software is free for use and redistribution while including this
-// license notice, unless:
-// 1. is used for commercial or non-personal purposes, or
-// 2. used for a product which includes or associated with a blockchain or other
-// decentralized database technology, or
-// 3. used for a product which includes or associated with the issuance or use
-// of cryptographic or electronic currencies/coins/tokens.
-// On all of the mentioned cases, an explicit and written permission is required
-// from the Author (Ohad Asor).
-// Contact ohad@idni.org for requesting a permission. This license may be
-// modified over time by the Author.
+// To view the license please visit https://github.com/IDNI/tau-lang/blob/main/LICENSE.txt
+
 #ifndef __IDNI__PARSER__MEASURE_H__
 #define __IDNI__PARSER__MEASURE_H__
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <map>
 
-#include "rewriting.h"
 
 namespace idni::measures {
 
