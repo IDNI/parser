5dd036358e194e55a08fd2ec255441bedfe83765
tklip <tomas@idni.org>
2026-08-07 09:18:33 +0200

    fix warning in clang

--- src/parser.tmpl.h
+++ src/parser.tmpl.h
@@ -430,7 +430,7 @@ void parser<C, T>::complete(const item& i, container_t& t, container_t& c,
 		// Predictor may have been evicted from S; it can still drive
 		// completion only if conjunction-cascade machinery kept it in U.
 		bool in_S = S[it->set].find(*it) != S[it->set].end();
-		bool in_U = any_conj && it->set < U.size()
+		bool in_U = any_conj && U.size() > it->set
 			&& U[it->set].find(*it) != U[it->set].end();
 		if (!in_S && !in_U) continue;
 		DBGP(print(std::cout << " ?  checking \t\t\t\t", *it) << "\n";)
