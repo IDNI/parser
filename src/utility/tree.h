5520288fe40bca71865944298b6f8f2186fdb4fb
tklip <tomas@idni.org>
2026-08-12 14:25:28 +0200

    move the tree handle types into tree_types.h and widen tref

--- src/utility/tree.h
+++ src/utility/tree.h
@@ -22,15 +22,11 @@
 #include <shared_mutex>
 #include <atomic>
 #include "../defs.h"
+#include "tree_types.h"
 
 namespace idni {
 
 //------------------------------------------------------------------------------
-
-// @brief tree node reference
-using tref  = const intptr_t*;
-// @brief vector of tree node reference handles
-using trefs = std::vector<tref>;
 
 // @brief range of tree node reference handles usable in range-based for loops
 template <typename T>
@@ -80,56 +76,16 @@ struct tree_range {
 
 //------------------------------------------------------------------------------
 
-template <typename T>
-struct bintree;
-
-// @brief tree handle wrapping a tref. htree::sp prevents tree from being gc-ed
-struct htree {
-	using sp = std::shared_ptr<htree>;
-	using wp = std::weak_ptr<htree>;
-	static const sp& null();
-	inline tref get() const;
-	inline bool operator==(const htree& r) const;
-	inline bool operator< (const htree& r) const;
-	//~htree();
-private:
-	tref hnd;
-	explicit htree(tref id = nullptr);
-	template <typename N> friend struct bintree;
-};
-
-using htref  = htree::sp;
-using htrefs = std::vector<htref>;
-
-//------------------------------------------------------------------------------
-
 // common predicates / callbacks
 inline static const auto all = [](const auto&) static { return true; };
 inline static const auto none = [](const auto&) static { return false; };
 inline static const auto identity = [](const auto& n) static { return n; };
 inline static const auto do_nothing = [](const auto&) static {};
 
-// helper type traits to check callback properties
-template<typename Cb>
-using bool_accepts_tref = std::is_invocable_r<bool, Cb, tref>;
-
-template<typename Cb>
-using bool_accepts_tref_tref = std::is_invocable_r<bool, Cb, tref, tref>;
-
-template<typename Cb>
-using accepts_tref_tref = std::is_invocable<Cb, tref, tref>;
-
-
 //------------------------------------------------------------------------------
 // rewriter types
 
 namespace rewriter {
-
-using rule = std::pair<htref, htref>;
-using rules = std::vector<rule>;
-
-using library = rules;
-using builder = rule;
 
 rules merge(const rules& rs1, const rules& rs2);
 
